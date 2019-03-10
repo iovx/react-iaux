bfd28ddc77cc59ae2aeca7afe158ff312a3453ff
ixw2017@163.com <Wsrf@2017@git$>
2019-03-11 00:45:25 +0800

    rebuild base files

--- typings/custom-typings.d.ts
+++ typings/custom-typings.d.ts
@@ -1,22 +1,26 @@
 declare module "*.txt" {
-    const content: string;
-    export default content;
+  const content: string;
+  export default content;
 }
 declare module "*.svg" {
-    const content: string;
-    export default content;
+  const content: string;
+  export default content;
 }
 // Some do it the other way around.
 declare module "*.json" {
-    const value: any;
-    export default value;
+  const value: any;
+  export default value;
 }
 declare module "*.less" {
-    const styles: any;
-    export default styles;
+  const styles: any;
+  export default styles;
 }
 
 declare module "react-iaux" {
-    export * from 'lib';
-    export * from 'lib/style';
+  export * from 'lib';
+  export * from 'lib/style';
+}
+
+declare module "marked" {
+  export * from "marked";
 }
