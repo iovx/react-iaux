2840f9e6876bdc0204f9eac21c255a0109ba6375
ibreeze2017 <ixw2017@163.com>
2019-03-11 15:16:17 +0800

    rebuild base file

---
+++ docs/source.js
@@ -0,0 +1,39 @@
+const data = `
+
+<div align="center">
+<img src="/entry.jpg" width='90%' alt="微风平台"/>
+</div>
+
+# api
+
+| 参数       | 说明           | 类型             | 默认值       |
+|------------|----------------|------------------|--------------|
+| labelStyle  | 左侧标签样式 | object | 无 |
+| inputStyle  | 输入框样式 | object | 无 |
+| rightLabelStyle  | 右侧标签样式 | object | 无 |
+| sharp | hex 的'#'标签 | boolean | false |
+
+
+\`\`\`js
+import React from 'react';
+import SyntaxHighlighter, {Light} from 'react-syntax-highlighter';
+import {androidstudio} from 'react-syntax-highlighter/dist/cjs/styles/hljs';
+import code from './source';
+
+const Component = () => {
+    
+    return (
+        <SyntaxHighlighter language='javascript' style={androidstudio}>
+            {code.source}
+        </SyntaxHighlighter>
+    )
+}
+
+export default Component;
+
+\`\`\``;
+
+
+export default {
+    source:data,
+}
