f466cc4848e28697b04373ad41719457ad7e1525
ibreeze2017 <ixw2017@163.com>
2019-03-15 13:39:11 +0800

    test

--- components/index.tsx
+++
@@ -1,9 +0,0 @@
-export {default as HttpUtil} from './http';
-export {default as CurrentTime} from './currentTime';
-export {default as Input} from './input';
-export {default as Form} from './form';
-export {default as Loading} from './loading';
-export {default as Button} from './button';
-export {default as Header} from './header';
-export {default as Card} from './card';
-export * from './util';
